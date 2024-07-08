236338b96ba4b81beb27c1540879f68f48fcb2d9
Stephen <stevemotif@gmail.com>
2024-07-08 00:53:02 -0400

    changes on backend

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
+const express  = require('express');
+const app = express()
 const mongoose = require('mongoose')
-const Product = require('../models/ProductModel');
-let app = express.Router()
+const Product = require('./models/ProductModel');
+const Job = require('./models/JobModel');
+const router = express.Router();
+
+//routes
+
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send(
+      "<div style='display:flex; justify-content:center;align-items:center;height:100vh;font-size:30px;color: steelblue; font-weight:bold; ' >Miracle Backend</div>"
+    );
+  });
+
+app.get('/hello', (req, res) => {
+    res.send('Hello node api')
+})
+
+
 
 
 //post a request
@@ -80,12 +99,41 @@ app.delete('/product/:id', async (req, res) => {
 })
 
 
-mongoose.connect('mongodb+srv://stevemotif:JEWle0f9UDrbXxzc@webapi.opv3m57.mongodb.net/?retryWrites=true&w=majority&appName=webapi')
-.then(() => {
-console.log('connected')
-}).catch((error) => {
-    console.log(error)
+app.post('/addjob', async (req,res) => {
+    try {
+        const job = await Job.create(req.body)
+        res.status(200).json(job)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
 })
 
 
-module.exports = app;
+// get request with id
+
+app.get('/job/:id', async ( req,res) => {
+    try {
+        const {id} = req.params;
+        const products =  await Job.findById(id);
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+mongoose.connect('mongodb+srv://stevemotif:JEWle0f9UDrbXxzc@webapi.opv3m57.mongodb.net/?retryWrites=true&w=majority&appName=webapi')
+.then(() => {
+
+console.log('connected')
+
+app.listen(3005, ()=> {
+    console.log(`Node API app is running on 3005`)
+})
+
+
+}).catch((error) => {
+    console.log(error)
+})
