2caa5d0a787d7dabcec1e4d1e5a2d14ccf228d7c
Stephen <stevemotif@gmail.com>
2024-07-14 11:58:18 -0400

    added CORS

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ const router = express.Router();
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.get("/", (req, res) => {
     res.send(
       "<div style='display:flex; justify-content:center;align-items:center;height:100vh;font-size:30px;color: steelblue; font-weight:bold; ' >Miracle Backend 1.0</div>"
