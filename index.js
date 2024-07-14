9a3208ebc8e4992c80713fd94e5d0ef73e3328c9
Stephen <stevemotif@gmail.com>
2024-07-14 13:05:58 -0400

    added log

--- index.js
+++ index.js
@@ -125,6 +125,7 @@ app.delete('/product/:id', async (req, res) => {
 
 app.post('/addjob', async (req,res) => {
     try {
+        console.log(req.body);
         const job = await Job.create(req.body)
         res.status(200).json(job)
 
